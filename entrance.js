2f87169a0a249e15d3b876d324b1a174c3faa82e
jingjingliu <liujingjing@hansintelligent.com>
2020-06-03 18:40:09 +0800

    .

--- entrance.js
+++ entrance.js
@@ -1,13 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-03 11:45:36
+ * @LastEditTime: 2020-06-03 17:42:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-admin-1\entrance.js
+ */
 import Vue from 'vue';
 
 // ElementUI
 import ElementUI from 'element-ui';
 Vue.use(ElementUI);
+// ElementUI
 
+// axios
 import http from './src/http/request'
 
+// element ui 框架进度条
 import 'nprogress/nprogress.css'
 
+//常用正则，方法
 import * as utils from './src/utils/util';
 
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-03 11:45:36
+ * @LastEditTime: 2020-06-03 18:39:33
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-admin-1\src\main.js
+ */
 import Vue from '../entrance';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+//
 import 'lib-flexible/flexible.js'
-import  '@/plugin/directive'
+import '@/plugin/directive'
 // 加载全局组件
 import './components';
 
